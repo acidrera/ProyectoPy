1ce468b0243ea14b2a3d5e39a5888ec6cc200582
acidrera <acidrera@xtec.cat>
2022-09-23 08:56:32 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+resultado = ""
+while resultado == "":
+    print("Ingresa un numero superior a 0")
+    numero = int(input())
+    if numero == 0:
+       print("Es un 0")
+    elif numero < 0:
+        print("Es un número negativo")
+    elif numero == 2:
+        resultado = "Los números primos son: " + str(2)
+    else:
+        resultado = "Los números primos son: "
+        acum = numero
+        i = 2
+        while acum>=2:
+            result = acum % i
+            if result == 0:
+                acum = acum / i
+                resultado = resultado + str(i) + "*"
+                i = 2
+            else:
+                i = i + 1
+print(resultado)
