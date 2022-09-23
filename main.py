fcaeb3b475549be90b46488340a4e95ae8140f7c
acidrera <acidrera@xtec.cat>
2022-09-23 10:35:07 +0200

    Test entre dispositivos

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 resultado = ""
 while resultado == "":
     print("Ingresa un numero superior a 0 para hacer la descomposición")
+    print("Introduce: ")
     numero = int(input())
     if numero == 0:
        print("Es un 0")
